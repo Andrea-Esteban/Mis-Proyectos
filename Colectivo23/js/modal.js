098c3be724841a20dd18d77a63a0527565658bbf
U201815353-AndreaCB <jeffthekiller3201@gmail.com>
2022-03-11 08:00:19 -0500

    Card yCarrusel

---
+++ Colectivo23/js/modal.js
@@ -0,0 +1,106 @@
+const open_modal = document.getElementById("open_modal");
+const open_modal2 = document.getElementById("open_modal2");
+const open_modal3 = document.getElementById("open_modal3");
+const open_modal4 = document.getElementById("open_modal4");
+const open_modal5 = document.getElementById("open_modal5");
+const open_modal6 = document.getElementById("open_modal6");
+const open_modal7 = document.getElementById("open_modal7");
+const open_modal8 = document.getElementById("open_modal8");
+
+const close_modal = document.getElementsByClassName("cerrar")[0];
+const close_modal2 = document.getElementsByClassName("cerrar")[1];
+const close_modal3 = document.getElementsByClassName("cerrar")[2];
+const close_modal4 = document.getElementsByClassName("cerrar")[3];
+const close_modal5 = document.getElementsByClassName("cerrar")[4];
+const close_modal6 = document.getElementsByClassName("cerrar")[5];
+const close_modal7 = document.getElementsByClassName("cerrar")[6];
+const close_modal8 = document.getElementsByClassName("cerrar")[7];
+
+const modal_content = document.querySelector(".modal_content");
+const info_modal = document.querySelector(".info_modal");
+
+const modal_content2 = document.querySelector(".class2content");
+const info_modal2 = document.querySelector(".class2info");
+
+const modal_content3 = document.querySelector(".class3content");
+const info_modal3 = document.querySelector(".class3info");
+
+const modal_content4 = document.querySelector(".class4content");
+const info_modal4 = document.querySelector(".class4info");
+
+const modal_content5 = document.querySelector(".class5content");
+const info_modal5 = document.querySelector(".class5info");
+
+const modal_content6 = document.querySelector(".class6content");
+const info_modal6 = document.querySelector(".class6info");
+
+const modal_content7 = document.querySelector(".class7content");
+const info_modal7 = document.querySelector(".class7info");
+
+const modal_content8 = document.querySelector(".class8content");
+const info_modal8 = document.querySelector(".class8info");
+
+open_modal.addEventListener("click", () => {
+  modal_content.classList.remove("hidden");
+  info_modal.classList.remove("hidden");
+});
+open_modal2.addEventListener("click", () => {
+  modal_content2.classList.remove("hidden");
+  info_modal2.classList.remove("hidden");
+});
+open_modal3.addEventListener("click", () => {
+  modal_content3.classList.remove("hidden");
+  info_modal3.classList.remove("hidden");
+});
+open_modal4.addEventListener("click", () => {
+  modal_content4.classList.remove("hidden");
+  info_modal4.classList.remove("hidden");
+});
+open_modal5.addEventListener("click", () => {
+  modal_content5.classList.remove("hidden");
+  info_modal5.classList.remove("hidden");
+});
+open_modal6.addEventListener("click", () => {
+  modal_content6.classList.remove("hidden");
+  info_modal6.classList.remove("hidden");
+});
+open_modal7.addEventListener("click", () => {
+  modal_content7.classList.remove("hidden");
+  info_modal7.classList.remove("hidden");
+});
+open_modal8.addEventListener("click", () => {
+  modal_content8.classList.remove("hidden");
+  info_modal8.classList.remove("hidden");
+});
+close_modal.addEventListener("click", () => {
+  modal_content.classList.add("hidden");
+  info_modal.classList.add("hidden");
+});
+close_modal2.addEventListener("click", () => {
+  modal_content2.classList.add("hidden");
+  info_modal2.classList.add("hidden");
+});
+close_modal3.addEventListener("click", () => {
+  modal_content3.classList.add("hidden");
+  info_modal3.classList.add("hidden");
+});
+close_modal4.addEventListener("click", () => {
+  modal_content4.classList.add("hidden");
+  info_modal4.classList.add("hidden");
+});
+close_modal5.addEventListener("click", () => {
+  modal_content5.classList.add("hidden");
+  info_modal5.classList.add("hidden");
+});
+close_modal6.addEventListener("click", () => {
+  modal_content6.classList.add("hidden");
+  info_modal6.classList.add("hidden");
+});
+close_modal7.addEventListener("click", () => {
+  modal_content7.classList.add("hidden");
+  info_modal7.classList.add("hidden");
+});
+close_modal8.addEventListener("click", () => {
+  modal_content8.classList.add("hidden");
+  info_modal8.classList.add("hidden");
+});
